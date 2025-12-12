5a2ce9edddaec9818693b87170b3434ce420ad8c
pxxja <poojabhambid2004@gmail.com>
2025-12-12 23:44:56 +0530

    change sameSite option for cookies

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json({
@@ -90,7 +90,7 @@ export const login=async(req,res)=>{
 
    res.cookie("token",token,{
     httpOnly:true,
-    sameSite:"none",
+    sameSite:"lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
    })
 
