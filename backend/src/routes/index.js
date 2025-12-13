c4f4114f37f3a9cc96db7277a7fbc17991a45153
pxxja <poojabhambid2004@gmail.com>
2025-12-13 21:13:48 +0530

    entry point for blogs

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth.route.js";
+import blogRouter from "./blog.route.js";
 
 const mainRouter=Router();
 
 mainRouter.use("/auth",authRouter)
+mainRouter.use("/blog",blogRouter)
+
 
 export default mainRouter;
