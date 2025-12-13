eb2c57a384aa7c2434b1c525fe932f72dbb214f6
pxxja <poojabhambid2004@gmail.com>
2025-12-13 23:34:42 +0530

    routing for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import ProtectedRoute from './componets/ProtectedRoute'
-import Home from './pages/Home'
+import { Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./componets/ProtectedRoute";
+import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
-import MyProfile from './pages/MyProfile'
+import MyProfile from "./pages/MyProfile";
+import AddBlog from "./pages/AddBlog";
+import MyBlogs from "./pages/MyBlogs";
+import EditBlog from "./componets/EditBlog";
 function App() {
-
   return (
     <>
-     <Toaster position="top-right" />
+      <Toaster position="top-right" />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-<Route path='/profile' element={<MyProfile/>}/>
+        <Route path="/profile" element={<MyProfile />} />
         <Route
           path="/dashboard"
           element={
@@ -24,9 +26,33 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/add-blog"
+          element={
+            <ProtectedRoute>
+              <AddBlog />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-blogs"
+          element={
+            <ProtectedRoute>
+              <MyBlogs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-blog/:blogId"
+          element={
+            <ProtectedRoute>
+              <EditBlog />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
