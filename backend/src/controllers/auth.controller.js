afba15431c6eebfee0854c037d328cac2688887d
pxxja <poojabhambid2004@gmail.com>
2025-12-14 14:39:43 +0530

    .

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure:true,
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json({
@@ -98,7 +98,7 @@ export const login=async(req,res)=>{
    res.cookie("token",token,{
     httpOnly:true,
     secure:true,
-    sameSite:"none",
+    sameSite:"lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
    })
 
