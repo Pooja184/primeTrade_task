aa03080091ce58ac41b1429fbb27f30fc4e1e879
pxxja <poojabhambid2004@gmail.com>
2025-12-14 15:25:46 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,24 +10,13 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-
-    if (origin.endsWith(".vercel.app")) {
-      return callback(null, true);
-    }
-
-    return callback(new Error("Not allowed by CORS"));
-  },
+  origin: true, 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
 
-app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
+app.use(cors(corsOptions)); 
 
-app.use(express.json());
-app.use(cookieParser());
 
 connectDB();
 
