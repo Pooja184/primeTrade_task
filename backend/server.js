ee11274f59233485f3de53ab03a32dad0c8a5563
pxxja <poojabhambid2004@gmail.com>
2025-12-14 13:02:34 +0530

    add deployed url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://primetrade-task-blog-application-1b3a.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
