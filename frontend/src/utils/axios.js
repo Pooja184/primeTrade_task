397a25a2d3216edd39f99a8b7656cf709a5b4d32
pxxja <poojabhambid2004@gmail.com>
2025-12-15 09:49:39 +0530

    axios instance

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// create axios instance to centralize API configuration
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL + "/api/v1",
   withCredentials: true,
